c0f8b6888cb2fe32b7eeda5cd537631246cd4e40
Lisa Mega Watts <64185677+DavinciDreams@users.noreply.github.com>
2025-03-21 17:39:36 +0000

    bot logic update

--- server/src/utils/botLogic.js
+++ server/src/utils/botLogic.js
@@ -1,18 +1,18 @@
 function addBotPlayers(players) {
-    const botCount = 4 - players.length
-    const botNames = ['Bot1', 'Bot2', 'Bot3', 'Bot4']
-    
-    for (let i = 0; i < botCount; i++) {
-      players.push({
-        id: `bot_${i}`,
-        name: botNames[i],
-        score: 0,
-        isBot: true
-      })
-    }
-    return players
+  const minPlayers = 4
+  const botsToAdd = minPlayers - players.length
+  const botPlayers = []
+
+  for (let i = 0; i < botsToAdd; i++) {
+    botPlayers.push({
+      id: `bot_${i}`,
+      name: `Bot${i + 1}`,
+      score: 0,
+      isBot: true
+    })
   }
-  
-  module.exports = {
-    addBotPlayers
-  }
+
+  return [...players, ...botPlayers]
+}
+
+module.exports = { addBotPlayers }
