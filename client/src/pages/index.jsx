8e26b7ec7d8ae00ce778cc27a426ad786e2032fa
Lisa Mega Watts <64185677+DavinciDreams@users.noreply.github.com>
2025-03-22 01:05:54 +0000

    fix dup player

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import io from 'socket.io-client';
 
@@ -10,9 +10,21 @@ const socket = io('https://acrophylia.onrender.com', {
 const Home = () => {
   const router = useRouter();
 
+  useEffect(() => {
+    socket.on('connect', () => console.log('Socket connected on index'));
+    socket.on('connect_error', (err) => console.log('Socket connect error on index:', err.message));
+    return () => {
+      socket.off('connect');
+      socket.off('connect_error');
+    };
+  }, []);
+
   const createRoom = () => {
+    console.log('Creating room');
     socket.emit('createRoom', 'one');
-    socket.on('roomCreated', (roomId) => {
+    socket.once('roomCreated', (roomId) => {
+      console.log('Room created received, roomId:', roomId);
+      sessionStorage.setItem('isCreator', 'true');
       router.push(`/room/${roomId}`);
     });
   };
